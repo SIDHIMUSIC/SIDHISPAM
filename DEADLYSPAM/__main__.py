d06630e7fcabca82675cdb1e34984b312a69daec
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2024-11-29 16:14:58 +0530

    Update __main__.py

--- DEADLYSPAM/__main__.py
+++ DEADLYSPAM/__main__.py
@@ -20,7 +20,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-deadly = "💙👀 Successfully Deployed TAXFCR SpamBot 💙👀 @BRANDED_WORLD Enjoy! Do visit @BRANDED_KHUSHI"
+deadly = "💙👀 Successfully Deployed TAXFCR SpamBot 💙👀 @SIDHI_SUPPORT Enjoy! Do visit @SIDHI_MUSIC"
 print(deadly[0: ])
 
 if __name__ == "__main__":
